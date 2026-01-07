7d6933c24e62d63861bdf17f35fcf1505b2fb7ea
artem <Artem-lazariev>
2026-01-07 20:08:51 +0200

    end

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+function homework() {
+    let me = {
+        age: 12,
+        name: "Artem",
+        isStudent: true,
+        myString: "hello",
+        mynumber: 10,
+        mynull: null
+    }
+    console.log(me.isStudent,me.mynumber + 10)
+    let username = prompt("Введіть своє ім’я")
+    let first= confirm("Ви дійсно хочете покинути сторінку?")
+    if (first) {
+        alert("Увага! Дія може бути небезпечною.")
+        let second = confirm("Ви впевнені, що хочете продовжити?")
+        if (second) {
+            window.location.href = "about:blank";
+        }
+    }
+}
+homework()
